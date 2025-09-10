f1f8300dc2d20ca7bb958d28b6d464074e668bf3
trungmac07 <mttrung16@gmail.com>
2025-09-10 20:57:52 +0700

    deploy using docker compose

--- Django_Server/convert.py
+++
@@ -1,7 +0,0 @@
-import torch 
-
-a = torch.load("/home/trungmac/code/projects/Law_Assistant/Django_Server/data/vectordb_128.pt", weights_only=False)
-
-a = [ i.to('cpu') for i in a]
-
-torch.save(a,"/home/trungmac/code/projects/Law_Assistant/Django_Server/data/new_vectordb_128.pt")
